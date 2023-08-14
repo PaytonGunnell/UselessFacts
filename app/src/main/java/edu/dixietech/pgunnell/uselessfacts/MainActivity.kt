d680a73e5fbb0c95ac8b5add6bbfc857d2ac3c0e
PaytonGunnell <paytongunnell@gmail.com>
2023-08-14 13:14:50 -0600

    Initial commit

---
+++ app/src/main/java/edu/dixietech/pgunnell/uselessfacts/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.dixietech.pgunnell.uselessfacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
